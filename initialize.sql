5f060148fcdf6e255938bd652f7aa37d821b90d9
Harishan Ravindranathan <harishanravindranathan@Harishans-MacBook-Pro-2.local>
2023-06-21 14:50:52 -0400

    changed package.json - run npm start on home directory

--- initialize.sql
+++ initialize.sql
@@ -1,9 +1,11 @@
 DROP DATABASE IF EXISTS foodData;
 CREATE DATABASE foodData;
 USE foodData;
-CREATE TABLE users(uid DECIMAL(3, 0) NOT NULL PRIMARY KEY, name VARCHAR(55));
-SELECT * FROM users;
+CREATE TABLE users(uid INT NOT NULL PRIMARY KEY, name VARCHAR(155));
+SELECT * FROM users 
+WHERE uid = 8000;
 
 create user 'cs348admin'@'localhost' identified by 'admin';
 grant all on foodData.* to 'cs348admin'@'localhost';	
 alter user 'cs348admin'@'localhost' identified with mysql_native_password by 'admin';
+
