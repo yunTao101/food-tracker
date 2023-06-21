95408416e73995cbd1233a6454615c730985bac6
harishan-r <harishan.ravin@gmail.com>
2023-06-21 14:58:05 -0400

    reverted back changes to sql initialize

--- initialize.sql
+++ initialize.sql
@@ -1,9 +1,8 @@
 DROP DATABASE IF EXISTS foodData;
 CREATE DATABASE foodData;
 USE foodData;
-CREATE TABLE users(uid INT NOT NULL PRIMARY KEY, name VARCHAR(155));
-SELECT * FROM users 
-WHERE uid = 8000;
+CREATE TABLE users(uid DECIMAL(3, 0) NOT NULL PRIMARY KEY, name VARCHAR(55));
+SELECT * FROM users;
 
 create user 'cs348admin'@'localhost' identified by 'admin';
 grant all on foodData.* to 'cs348admin'@'localhost';	
