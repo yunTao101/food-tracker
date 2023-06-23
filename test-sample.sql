c9b69b098a2208cbf4f514cb60c0e419a0251182
harishan-r <harishan.ravin@gmail.com>
2023-06-22 21:56:48 -0400

    done test-samples

--- test-sample.sql
+++ test-sample.sql
@@ -1,18 +1,19 @@
 DROP DATABASE IF EXISTS testFoodData;
 CREATE DATABASE testFoodData;
 USE testFoodData;
+SET SQL_SAFE_UPDATES = 0;
 
 -- Creating all tables
 CREATE TABLE Users (
-    uID INT NOT NULL, 
+    uID INT NOT NULL AUTO_INCREMENT, 
     accountType VARCHAR(5) NOT NULL, 
     firstName VARCHAR(100),
     lastName VARCHAR(100),
     username VARCHAR(100) UNIQUE,
     password VARCHAR(100),
     email VARCHAR(255) UNIQUE,
-    age INT,
     gender VARCHAR(1),
+    age INT,
     weight FLOAT,
     height INT,
     desiredWeight FLOAT,
@@ -66,12 +67,12 @@ CREATE TABLE EatenCustomMeals(
 
 
 -- Testing User Table
-INSERT INTO Users VALUES (1, "Admin", "Admin", "Account", "admin1", "admin1", "admin1FoodTracker@gmmail.com", null, null, null, null, null, null);
-INSERT INTO Users VALUES (2, "User", "Akshen", "Jasikumar", "akshen28", "akshen123", "akshen.jasikumar@gmmail.com", 21, "M", 160.0, 69, 150.0, 1800);
-INSERT INTO Users VALUES (3, "User", "Yun", "Tao", "yunTao", "yun123", "yuntao200@gmail.com", 21, "M", 170.0, 71, 165.0, 2000);
+INSERT INTO Users (accountType, firstName, lastName, username, password, email, gender, age, weight, height, desiredWeight, caloricGoal) VALUES ("Admin", "Admin", "Account", "admin1", "admin1", "admin1FoodTracker@gmmail.com", null, null, null, null, null, null);
+INSERT INTO Users (accountType, firstName, lastName, username, password, email, gender, age, weight, height, desiredWeight, caloricGoal) VALUES ("User", "Akshen", "Jasikumar", "akshen28", "akshen123", "akshen.jasikumar@gmmail.com", "M", 21, 160.0, 69, 150.0, 1800);
+INSERT INTO Users (accountType, firstName, lastName, username, password, email, gender, age, weight, height, desiredWeight, caloricGoal) VALUES ("User", "Yun", "Tao", "yunTao", "yun123", "yuntao200@gmail.com", "M", 21, 170.0, 71, 165.0, 2000);
 -- Insert New Account
 Select * FROM Users;
-INSERT INTO Users VALUES (4, "User", "Sahi", "Abdullah", "sahiA12", "abd32", "abd270072@gmail.com", 21, "M", 140.0, 68, 150.0, 2200);
+INSERT INTO Users (accountType, firstName, lastName, username, password, email, gender, age, weight, height, desiredWeight, caloricGoal) VALUES ("User", "Sahi", "Abdullah", "sahiA12", "abd32", "abd270072@gmail.com", "M", 21, 140.0, 68, 150.0, 2200);
 Select * FROM Users;
 -- Delete Account
 Select * FROM Users;
@@ -81,6 +82,8 @@ Select * FROM Users;
 Select * FROM Users;
 UPDATE Users SET age = 22 WHERE uID = 4;
 Select * FROM Users;
+-- Login Check
+SELECT * FROM Users WHERE username = "akshen28" AND password = "akshen123";
 
 
 -- Testing FoodIngredients Table
@@ -128,28 +131,32 @@ UPDATE FoodCustomMeals SET quantity = 2 WHERE foodID = 30 AND mealID = 1;
 SELECT * FROM FoodCustomMeals ORDER BY mealID;
 
 
--- -- Testing EatenIngredients
--- -- PUT THE INSERTS HERE  <-----------------------------
--- -- Select how many times a user ate a particular ingredient given a date range
--- SELECT * FROM EatenIngredients WHERE date BETWEEN "2023-06-20 00:00:00" AND "2023-06-20 23:59:59" AND foodID = 254 AND uID = 2;
--- -- Marking Ingredient as Eaten
--- Select * FROM EatenIngredients;
--- INSERT INTO EatenIngredients VALUES (421, 5, GETDATE());
--- Select * FROM EatenIngredients;
--- -- Removing Ingredient as Eaten
--- Select * FROM EatenIngredients;
--- DELETE FROM EatenIngredients WHERE foodID = 421 AND date = "2023-06-21 20:01:33.817" AND uID = 5;
--- Select * FROM EatenIngredients;
+-- Testing EatenIngredients
+INSERT INTO EatenIngredients (foodID, uID, date) VALUES (30, 1, "2023-06-19");
+INSERT INTO EatenIngredients (foodID, uID, date) VALUES (54, 2, "2023-06-20");
+INSERT INTO EatenIngredients (foodID, uID, date) VALUES (8, 4, "2023-06-20");
+-- Select what a user ate in a given date range
+SELECT * FROM EatenIngredients WHERE date BETWEEN "2023-06-20" AND "2023-06-20" AND uID = 2;
+-- Marking Ingredient as Eaten
+Select * FROM EatenIngredients;
+INSERT INTO EatenIngredients (foodID, uID, date) VALUES (973, 2, "2023-06-22");
+Select * FROM EatenIngredients;
+-- Removing Ingredient as Eaten
+Select * FROM EatenIngredients;
+DELETE FROM EatenIngredients WHERE foodID = 973 AND date = "2023-06-22" AND uID = 2;
+Select * FROM EatenIngredients;
 
 
--- -- Testing EatenCustomMeal
--- -- PUT THE INSERTS HERE <-----------------------------
--- -- Adding Meal eaten
--- Select * from EatenCustomMeal;
--- INSERT INTO EatenCustomMeal VALUES (25, 5, GETDATE());
--- Select * from EatenCustomMeal;
--- -- Deleting Meal eaten
--- Select * from EatenCustomMeal;
--- DELETE FROM EatenCustomMeal WHERE mealID = 25 AND date = "2023-06-23 20:01:33.817" AND uID = 5;
--- Select * from EatenCustomMeal;
+-- Testing EatenCustomMeals
+INSERT INTO EatenCustomMeals (mealID, uID, date) VALUES (3, 2, "2023-06-19");
+INSERT INTO EatenCustomMeals (mealID, uID, date) VALUES (1, 1, "2023-06-20");
+INSERT INTO EatenCustomMeals (mealID, uID, date) VALUES (2, 4, "2023-06-21");
+-- Adding Meal eaten
+Select * from EatenCustomMeals;
+INSERT INTO EatenCustomMeals (mealID, uID, date) VALUES (4, 4, "2023-06-21");
+Select * from EatenCustomMeals;
+-- Deleting Meal eaten
+Select * from EatenCustomMeals;
+DELETE FROM EatenCustomMeals WHERE mealID = 4 AND date = "2023-06-21" AND uID = 4;
+Select * from EatenCustomMeals;
 
